cb903235d5c83839b290b6589670f57429e87393
Matthijs Steen <emailmatthijs@gmail.com>
2021-10-21 17:55:05 +0200

    initial commit

---
+++ src/bin.ts
@@ -0,0 +1,33 @@
+import * as globBase from "glob-base"
+import { dirname, join } from "path"
+import { resolveConfig, resolveConfigFile } from "prettier"
+import { exit } from "process"
+import { Args, editFiles, parseArgs } from "trim-trailing"
+
+async function main(args: Args) {
+  const base = globBase(args.glob).base
+  if (!args.ignoreFile) {
+    const prettierConfigFile = await resolveConfigFile(base)
+    if (prettierConfigFile) {
+      args.ignoreFile = join(dirname(prettierConfigFile), ".prettierignore")
+    }
+  }
+  const options = await resolveConfig(base)
+  if (!options) {
+    console.error("Could not find prettier options.", base)
+    exit(1)
+  }
+  const wrongIndent = indenter(!options.useTabs)
+  const rightIndent = indenter(options.useTabs)
+  const indentRegex = new RegExp("^(?:" + wrongIndent + ")+", "gm")
+  return editFiles(args, (contents) =>
+    contents
+      .replace(indentRegex, (match) => rightIndent.repeat(match.length / wrongIndent.length))
+      .replace(/[ \t]+$/gm, ""),
+  )
+}
+main(parseArgs("prettier-strings"))
+
+function indenter(options: any, useTabs: boolean = options.useTabs): string {
+  return useTabs ? "\t" : " ".repeat(options.tabWidth)
+}
